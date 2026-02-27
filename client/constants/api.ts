d5faf27575ed11cb5425e6bd3a4110fdaa8c3d41
abhishekkaldate <abhishekkaldate31@gmail.com>
2026-02-27 15:52:42 +0530

    fix last bug

--- client/constants/api.ts
+++ client/constants/api.ts
@@ -1,12 +1,5 @@
 import axios from "axios";
-import { Platform } from "react-native";
 
-const LOCAL_API_URL = Platform.select({
-    android: "http://10.253.3.28:3000/api",
-    ios: "http://10.253.3.28:3000/api",
-    default: "http://localhost:3000/api"
-})
-
-const api = axios.create({baseURL: LOCAL_API_URL})
+const api = axios.create({baseURL: "https://barber-server-seven.vercel.app/api"})
 
 export default api;
